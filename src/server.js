bbc528cf9bac3be6b8f6b4972d24fd430ab8a406
fahadmughal3737 <fahadmughal3737@gmail.com>
2026-03-03 16:41:06 +0500

    continue from relations

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 // const express = require('express')
 import express from 'express'
+import { config } from 'dotenv';
+import { connectDB, disconnectDB } from './config/db.js';
 const app = express()
 import movierouter from './routes/movieRoutes.js';
 import authRouter from './routes/auth.js';
+config()
+connectDB()
 const port = 3000
 
 app.use('/movies', movierouter)
